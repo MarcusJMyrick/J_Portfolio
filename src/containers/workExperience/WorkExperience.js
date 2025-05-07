cd87bb313aae3dd98096928a9e0169df4152da3c
MarcusJMyrick <mjmyrick12@gmail.com>
2025-05-06 23:08:36 -0500

    `Updated Header.scss to increase max-height of menu and added mobile check in WorkExperience.js to conditionally render content`

--- src/containers/workExperience/WorkExperience.js
+++ src/containers/workExperience/WorkExperience.js
@@ -7,34 +7,42 @@ import { StyleContext } from "../../contexts/StyleContext";
 
 export default function WorkExperience() {
   const {isDark} = useContext(StyleContext);
+  const isMobile = typeof window !== 'undefined' && window.innerWidth <= 768;
+
+  console.log('workExperiences:', workExperiences);
+  console.log('workExperiences.experience:', workExperiences.experience);
   if (workExperiences.display) {
+    const content = (
+      <div className="experience-container" id="workExperience">
+        <div>
+          <h1 className="experience-heading">Experiences</h1>
+          <div className="experience-cards-div">
+            {workExperiences.experience.length === 0 && (
+              <div style={{color: 'red', textAlign: 'center'}}>No experience cards found</div>
+            )}
+            {workExperiences.experience.map((card, i) => {
+              return (
+                <ExperienceCard
+                  key={i}
+                  isDark={isDark}
+                  cardInfo={{
+                    company: card.company,
+                    desc: card.desc,
+                    date: card.date,
+                    companylogo: card.companylogo,
+                    role: card.role,
+                    descBullets: card.descBullets
+                  }}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
     return (
       <div id="experience">
-        <Fade bottom duration={1000} distance="20px">
-          <div className="experience-container" id="workExperience">
-            <div>
-              <h1 className="experience-heading">Experiences</h1>
-              <div className="experience-cards-div">
-                {workExperiences.experience.map((card, i) => {
-                  return (
-                    <ExperienceCard
-                      key={i}
-                      isDark={isDark}
-                      cardInfo={{
-                        company: card.company,
-                        desc: card.desc,
-                        date: card.date,
-                        companylogo: card.companylogo,
-                        role: card.role,
-                        descBullets: card.descBullets
-                      }}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-        </Fade>
+        {isMobile ? content : <Fade bottom duration={1000} distance="20px">{content}</Fade>}
       </div>
     );
   }
